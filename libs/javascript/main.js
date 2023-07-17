577abc118cf5393e4cc8a438d38b875b8dd12218
Steven Brooks <brookscodeblog@gmail.com>
2023-07-17 14:56:24 +0700

    fuck you

--- libs/javascript/main.js
+++ libs/javascript/main.js
@@ -3,6 +3,7 @@
 ========================================================*/
 
 let personnelArr = null;
+let departmentArr = null;
 let tbody = $("table tbody");
 // let delBtn = $('.del-btn');
 
@@ -28,10 +29,10 @@ $(document).ready(function() {
             readyRes.data.forEach(item => {
                 personnelArr.push(item);
             })
-            personnelArr.forEach(function(item, index) {
+            personnelArr.forEach(function(item) {
                 $(".db-body").append(`
                 <tr class="emp-row">
-                    <td class="number">${index + 1}</td>
+                    <td class="number">${item.id}</td>
                     <td class="surname">${item.lastName}</td>
                     <td class="firstname">${item.firstName}</td>
                     <td class="email">${item.email}</td>
@@ -54,36 +55,67 @@ $(document).ready(function() {
     ARRANGE DATA WITH HEADING ARROWS
 =====================================================*/
 
+// for this, need to make calls to backend to reverse things, then send back to front end to manifest
+
 $(".db-index").click(function() {
     tbody.html($('tr',tbody).get().reverse());
 })
 
-$(".db-surname").click(function() {
-    tbody.html($('tr',tbody).get().reverse());
-})
 
-$(".db-firstname").click(function() {
-    console.log($(".db-body")[0]);
-    $(".db-body").sort(function(item1, item2) {
-        return item1.firstName < item2.firstName ? -1 : 1;
-    })
-    
-})
 
 
 /*====================================================
                 CALLS TO PHP
 ======================================================*/
 
+/*===============DELETE PERSONNEL BY ID==============*/
 tbody.on("click", ".del-btn", function(e) {
     console.log(e.target.parentElement.parentElement.children[0].innerHTML);
+    console.log(e.target.parentElement.parentElement);
     let id = e.target.parentElement.parentElement.children[0].innerHTML;
+    let persRow = $(e.target.parentElement.parentElement);
 
     $.ajax({
-        "url": `libs/php/deletePersonnelByID?id=${id}`,
+        "url": `libs/php/deletePersonnelByID.php?id=${id}`,
         "type": "DELETE",
         "success": function(result) {
             console.log(result);
+            console.log(personnelArr);
+            persRow.slideUp();
         }
     })
 })
+
+/*===================GET ALL DEPTS======================*/
+$(".dropdown-departments").click(function() {
+
+    $.ajax({
+        "url": "libs/php/getAllDepartments.php",
+        "type": "GET",
+        "success": function(result) {
+            departmentArr = result.data;
+            console.log(departmentArr);
+            departmentArr.forEach(function(item) {
+                $(".db-body").append(`
+                <tr class="emp-row">
+                    <td class="number">${item.id}</td>
+                    <td class="surname">${item.name}</td>
+                    <td class="firstname">${item.locationID}</td>
+                    <td class="modify"><button type="button" class="btn btn-success edit-btn">Edit</button>
+                    <button type="button" class="btn btn-danger del-btn">Delete</button></td>
+                </tr>
+                `); 
+            })
+        }
+    })
+
+})
+
+
+
+
+
+
+
+
+// need to figure out how to restore original db and make a copy of this for when user refresh page, etc
